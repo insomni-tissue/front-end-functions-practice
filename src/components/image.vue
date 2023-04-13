51cad22d0cd8db8fe1eae98c7c733fa56cdf31ab
onion <onion@on.com>
2023-04-13 22:52:51 +0800

    add imageUdload

--- src/components/image.vue
+++ src/components/image.vue
@@ -4,12 +4,14 @@
   </div>
 </template>
 <script setup lang="ts">
-import { getImageData } from 'front-end-functions';
+import { getImageData, downloadImage, downloadImageTwo } from 'front-end-functions';
 const upload = (e: any) => {
   getImageData(e).then((res) => {
     console.log('res', res)
   })
 }
+downloadImage('/src/assets/bg.jpeg', 'test001', 'jpeg')
+downloadImageTwo('/src/assets/bg.jpeg', 'test05', 'jpeg')
 </script>
 <style scoped>
 </style>
